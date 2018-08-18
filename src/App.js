13e065b4369f27f65efcbc20df1faf520ab559dc
GzhiYi <gzy.guy@gmail.com>
2018-08-18 15:36:59 +0800

    添加vscode调试launch.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClick (e) {
+    console.log('e', e);
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro" onClick={this.handleClick}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
