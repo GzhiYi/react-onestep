7f650d63cff2395484f3ec011c51d208a4e10b06
GzhiYi <gzy.guy@gmail.com>
2018-08-18 16:43:39 +0800

    增加flowi静态类型检查

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  handleClick (e) {
-    console.log('e', e);
-  }
   render() {
     return (
       <div className="App">
@@ -13,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro" onClick={this.handleClick}>
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
